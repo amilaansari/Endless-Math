775e89627bafcd8945b13ee6b48b69918d859ca2
amilaansari <77079679+amilaansari@users.noreply.github.com>
2021-07-11 12:42:15 +0800

    Add files via upload

---
+++ endlessMath.py
@@ -0,0 +1,5 @@
+#while loop till exit/how many questions would you like?
+#generate questions and answers
+#if usr_answer == sys_answer, move onto next question
+#else stay on question
+#at the end, total correct answers and ask to play again or exit
