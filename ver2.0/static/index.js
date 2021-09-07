ec4294389e2c556e9b81f74a27c6c26c6befdaa6
amila <amilaansari@gmail.com>
2021-09-07 12:40:44 +0800

    change jQuery to JavaScript for ease

--- ver2.0/static/index.js
+++ ver2.0/static/index.js
@@ -1,11 +1,8 @@
-//on load
-$(function(){
-    //retrieve userName Input
-    $("#start_btn").on("click",function(){
-        const userName = $("#user_input_name").val()
-        alert ("Welcome"+ userName + "!")
-    })
-    
-});
-    
+//listen for click
+document.getElementById("start_btn").addEventListener("click", function(){
+    //retrieve userName data
+    const userName = document.getElementById("user_input_name").value;
+    //print userName data
+    alert ("Welcome " + userName + "!")
+})
     
