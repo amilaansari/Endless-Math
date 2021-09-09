6863920885fe58305bd8f2700fd0f89cf5c86ee2
amila <amilaansari@gmail.com>
2021-09-09 10:36:52 +0800

    restructure directory, new project layout, simplified project

---
+++ ver2.0 website/static/index.js
@@ -0,0 +1,9 @@
+//welcomeUser function
+function welcomeUser(){
+    alert("clicked")
+    // //retrieve userName data
+    // const userName = document.getElementById("user_input_name").value;
+    // //print userName data
+    // document.getElementById("welcomeUser").text = "Welcome " + userName + "!";
+}
+    
--- ver2.0/static/index.js
+++
@@ -1,8 +0,0 @@
-//listen for click
-document.getElementById("start_btn").addEventListener("click", function(){
-    //retrieve userName data
-    const userName = document.getElementById("user_input_name").value;
-    //print userName data
-    alert ("Welcome " + userName + "!")
-})
-    
