c0fa22f8d6273b2b586a52404270e78a8db50f20
amila <amilaansari@gmail.com>
2021-09-10 12:23:05 +0800

    print userName data in intro para

--- docs/static/index.js
+++ docs/static/index.js
@@ -1,9 +1,9 @@
 //welcomeUser function
 function welcomeUser(){
-    alert("clicked")
-    // //retrieve userName data
-    // const userName = document.getElementById("user_input_name").value;
-    // //print userName data
-    // document.getElementById("welcomeUser").text = "Welcome " + userName + "!";
+    //alert("clicked")
+    //retrieve userName data
+    var userName = document.getElementById("user_input_name").value;
+    //print userName data
+    document.getElementById("intro").innerHTML = "Welcome " + userName + "!";
 }
     
