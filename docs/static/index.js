54205003985eb7d2163f14ea6c663fb0586d8650
amila <amilaansari@gmail.com>
2021-09-10 13:08:05 +0800

    generate and display random numbers within limits set by user

--- docs/static/index.js
+++ docs/static/index.js
@@ -6,4 +6,23 @@ function welcomeUser(){
     //print userName data
     document.getElementById("intro").innerHTML = "Welcome " + userName + "!";
 }
-    
+var highNum = document.getElementById("top_limit").value
+var lowNum = document.getElementById("bot_limit").value
+    //if user inputs 0
+    if (lowNum == 0){
+        lowNum += 1
+    }
+
+function startGame(){
+
+    //generate random numbers within limits set by user
+    var x = Math.floor((Math.random() * highNum)+ lowNum)
+    var y = Math.floor((Math.random() * highNum)+ lowNum)
+    var z = x + y
+
+    //print equation
+    document.getElementById("x").value = x
+    document.getElementById("y").value = y
+    document.getElementById("z").value = z
+    // document.getElementById("game").innerHTML = x + y
+}
